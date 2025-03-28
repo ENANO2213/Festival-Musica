237d2fe490a8ea7cce5ed57eebb71b5ace7b63fa
Brian Arias <arias221308@gmail.com>
2025-03-27 21:36:24 -0600

    Se añade efectos de scroll en la vista y se finaliza el proyecto

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,23 @@
 //Espera a que todo el HTML y CSS sea cargado para la ejecución de la función
 document.addEventListener("DOMContentLoaded", function () {
+  //Llamado a función de Navegación fija
+  navFija();
   crearGaleria();
+  resaltarMenu();
+  scrollNav();
 });
+//Función para la navegación fija (solo en dispositivos desktop en adelante)
+function navFija() {
+  const header = document.querySelector(".header");
+  const sobreFestival = document.querySelector(".sobre-festival");
+  document.addEventListener("scroll", function () {
+    if (sobreFestival.getBoundingClientRect().bottom < 1) {
+      header.classList.add("fixed");
+    } else {
+      header.classList.remove("fixed");
+    }
+  });
+}
 //Función para crear la galeria
 function crearGaleria() {
   const galeria = document.querySelector(".galeria-imagenes");
@@ -44,7 +60,7 @@ function mostrarImagen(i) {
 //Función para cerrar el Modal
 function cerrarModal() {
   const modal = document.querySelector(".modal");
-  if (!modal) return;//Se verifica si existe o no el modal, si existe lo muestra, sino no
+  if (!modal) return; //Se verifica si existe o no el modal, si existe lo muestra, sino no
 
   modal.classList.add("fade-out");
 
@@ -53,4 +69,42 @@ function cerrarModal() {
     const body = document.querySelector("body");
     body.classList.remove("overflow-hidden");
   }, 490);
+}
+
+//Función para reasaltar el menú de acuerdo a ubicación en la vista
+function resaltarMenu() {
+  document.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll("section");
+    const links = document.querySelectorAll(".navegacion-principal a");
+
+    let actual = "";
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        actual = section.id;
+      }
+    });
+    links.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === "#" + actual) {
+        link.classList.add("active");
+      }
+    });
+  });
+}
+
+//Función para agregar un efecto a la navegación
+function scrollNav() {
+  const navLinks = document.querySelectorAll(".navegacion-principal a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const sectionScroll = e.target.getAttribute("href");
+      const section = document.querySelector(sectionScroll);
+
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
 }
